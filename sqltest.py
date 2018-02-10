6aa73f0d0cc16a997364402582495b82185799e4
emjin <emmaljin@yahoo.com>
2018-02-10 11:49:32 -0500

    Made the website work

--- sqltest.py
+++ sqltest.py
@@ -6,4 +6,8 @@ password = 'T1n4andEmm4andM1r4nd4!'
 driver = '{ODBC Driver 13 for SQL Server}'
 cnxn = cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
-:
+cursor.execute("SELECT * FROM programs")
+row = cursor.fetchone()
+while row:
+    print (str(row[0]) + " " + str(row[1]))
+    row = cursor.fetchone()
